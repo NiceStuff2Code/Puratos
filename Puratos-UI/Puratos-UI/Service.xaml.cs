3452ff20e0d5d3d135f362b585bf4f9089b43c05
Mathias Bolle <34421646+NiceStuff2Code@users.noreply.github.com>
2020-04-15 14:38:22 +0200

    error messages verwijderd.

--- Puratos-UI/Puratos-UI/Service.xaml.cs
+++ Puratos-UI/Puratos-UI/Service.xaml.cs
@@ -7,12 +7,12 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace Puratos-UI
+namespace Puratos_UI
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
-public partial class Page1 : ContentPage
+public partial class Service : ContentPage
 {
-    public Page1()
+    public Service()
     {
         InitializeComponent();
     }
